66b666b4010702aed278c33d56eb34a32eba7068
Bruno Pagno <brunopagno@gmail.com>
2019-08-07 23:51:11 +0200

    no more classes left

--- src/views/StoriesView.jsx
+++ src/views/StoriesView.jsx
@@ -1,44 +1,31 @@
-import React, { Fragment } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import StoriesService from '../services/StoriesService';
 import Stories from '../components/Stories';
 import Input from '../components/Input';
 
-class StoriesView extends React.Component {
-  constructor(props) {
-    super(props);
+const StoriesView = () => {
+  const [stories, setStories] = useState([]);
 
-    this.state = {
-      stories: [],
-    };
+  useEffect(() => {
+    async function fetchStories() {
+      const retrievedStories = await StoriesService.getStories(); 
+      setStories(retrievedStories);
+    }
+    fetchStories();
+    console.log("RUN EFFECT");
+  }, []);
 
-    this.addStory = this.addStory.bind(this);
-  }
-
-  async componentDidMount() {
-    const stories = await StoriesService.getStories();
-    this.setState({
-      stories,
-    });
-  }
-
-  async addStory(text) {
-    const { stories } = this.state;
+  const addStory = async (text) => {
     const response = await StoriesService.addStory(text);
-    this.setState({
-      stories: [...stories, { id: response.id, text: response.text }],
-    });
-  }
+    setStories([...stories, { id: response.id, text: response.text }]);
+  };
 
-  render() {
-    const { stories } = this.state;
-
-    return (
-      <Fragment>
-        <Input onSubmit={this.addStory} />
-        <Stories stories={stories} />
-      </Fragment>
-    );
-  }
-}
+  return (
+    <Fragment>
+      <Input onSubmit={addStory} />
+      <Stories stories={stories} />
+    </Fragment>
+  );
+};
 
 export default StoriesView;
